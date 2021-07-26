9ea0d1695550ac0b69b563bc400a54e38e4c00d5
Quentin <pamartqu@gmail.com>
2021-07-27 00:54:11 +0200

    commit to laptop

--- MinesweeperServer/src/main.cpp
+++ MinesweeperServer/src/main.cpp
@@ -1,6 +1,17 @@
+#include "SFML/Network.hpp"
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    char data[100];
+    std::size_t received;
+    sf::UdpSocket socket;
+
+    sf::IpAddress sender;
+    unsigned short port;
+    if(auto err = socket.receive(data, 100, received, sender, port) != sf::Socket::Done){
+        std::cerr << "Error receiving socket data : " << std::endl;
+        std::cerr << err << std::endl;
+    }
     return 0;
 }
